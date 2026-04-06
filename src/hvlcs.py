8ab41c8a1f7e6838439566f1fa4998636414a2ad
Kylen Williams <kylenwilliams99@gmail.com>
2026-04-06 18:54:34 -0400

    fixes the input stuff

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -4,12 +4,6 @@
 # max(opt[i-1][j] + vA[i], opt[i][j-1] + vA[j])	if Ai != Bj / otherwise
 
 def hvlcs(A, B, vA):
-    value_set = sorted(set(A) & set(B))
-
-    value_dict = {}
-    for i in range(len(value_set)):
-        value_dict[value_set[i]] = vA[i]
-
     n = len(A)
     m = len(B)
 
@@ -18,59 +12,33 @@ def hvlcs(A, B, vA):
     for i in range(n - 1, -1, -1):
         for j in range(m - 1, -1, -1):
             if A[i] == B[j]:
-                opt[i][j] = opt[i + 1][j + 1] + value_dict[A[i]]
+                opt[i][j] = opt[i + 1][j + 1] + vA[A[i]]
             else:
                 opt[i][j] = max(
                     opt[i + 1][j],
                     opt[i][j + 1]
                 )
 
-    return opt[0][0]
+    return opt, opt[0][0]
 
 # def backtracking(A, B, vA, vB, opt):
 
 #     return 0
 
 if __name__ == "__main__":
-    A = "aacb"
-    B = "caab"
-    vA = [2, 4, 5]
+    letter_value_dict = {}
 
-    # letter_value_dict = {}
+    alphabet = int(input("Input number of characters in alphabet: "))
+    for i in range(alphabet):
+        letter_value = list(map(str, input().split()))
 
-    # alphabet = int(input("Input number of characters in alphabet: "))
-    # for i in range(alphabet):
-    #     letter_value = list(map(str, input().split()))
-
-    #     letter_value[0] = letter_value[0].lower()
-    #     letter_value[1] = int(letter_value[1])
-    #     letter_value_dict[letter_value[0]] = letter_value[1]
-
-    # vA=letter_value_dict
+        letter_value[0] = letter_value[0].lower()
+        letter_value[1] = int(letter_value[1])
+        letter_value_dict[letter_value[0]] = letter_value[1]
 
 
-    # A = input("Input string A: ")
-    # B = input("Input string B: ")
+    A = input("Input string A: ")
+    B = input("Input string B: ")
 
-    print(hvlcs(A, B, vA))
-
-
-    # for i in range(len(value_set)):
-    #     value_dict[value_set[i]] = vA[i]
-
-    # n = len(A)
-    # m = len(B)
-    # # opt array
-    # opt = [[0 for _ in range(m)] for _ in range(n)]
-    # for i in range(1, n):
-    #     for j in range(1, m):
-    #         if A[i] == B[j]:
-    #             if j < m and i < n:
-    #                 opt[i][j] = opt[i + 1][j + 1] + value_dict[A[i]]
-    #             else:
-    #                 opt[i][j] = opt[i][j] + value_dict[A[i]]
-    #         else:
-    #             if j < m and i < n:
-    #                 opt[i][j] = max(opt[i + 1][j] + value_dict[A[i]], opt[i][j + 1] + value_dict[B[j]])
-    #             else:                    
-    #                 opt[i][j] = max(opt[i][j] + value_dict[A[i]], opt[i][j] + value_dict[B[j]])
+    opt_array, opt_value = hvlcs(A, B, letter_value_dict)
+    
